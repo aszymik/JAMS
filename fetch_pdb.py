cc06097b92036d35faa1c2bae5d11689ba30bdc2
annnie73 <aniaszymik02@gmail.com>
2025-05-27 17:12:48 +0200

    fetch structures from pdb

---
+++ fetch_pdb.py
@@ -0,0 +1,16 @@
+import requests
+
+pdb_id = '9E2J'
+filename = '9E2J.pdb'
+file_format = 'pdb'  # pdb or cif
+
+url = f'https://files.rcsb.org/download/{pdb_id.lower()}.{file_format}'
+response = requests.get(url)
+
+if response.status_code == 200:
+    filename = f'{pdb_id.upper()}.{file_format}'
+    with open(filename, 'w') as f:
+        f.write(response.text)
+    print(f'PDB structure saved as {filename}')
+else:
+    print(f"Failed to download structure for ID '{pdb_id.upper()}'. HTTP status code: {response.status_code}")
