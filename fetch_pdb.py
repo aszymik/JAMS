8b12d103053f4ad9b3af3c5822b9bc03d84a4827
annnie73 <aniaszymik02@gmail.com>
2025-05-27 17:16:23 +0200

    fix bugs

--- fetch_pdb.py
+++ fetch_pdb.py
@@ -8,7 +8,6 @@ url = f'https://files.rcsb.org/download/{pdb_id.lower()}.{file_format}'
 response = requests.get(url)
 
 if response.status_code == 200:
-    filename = f'{pdb_id.upper()}.{file_format}'
     with open(filename, 'w') as f:
         f.write(response.text)
     print(f'PDB structure saved as {filename}')
