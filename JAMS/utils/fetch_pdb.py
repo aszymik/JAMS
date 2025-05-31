9e7d462552e2e144756dc6f69a6f0fc7245a786b
annnie73 <aniaszymik02@gmail.com>
2025-05-31 12:26:13 +0200

    update fetching pdb files

--- JAMS/utils/fetch_pdb.py
+++ JAMS/utils/fetch_pdb.py
@@ -1,15 +1,31 @@
 import requests
 
-pdb_id = '9E2J'
-filename = '9E2J.pdb'
-file_format = 'pdb'  # pdb or cif
+def get_pdb(pdb_id, output_dir, file_format='pdb'):
+    """
+    Download a PDB or CIF structure file from the RCSB PDB repository.
 
-url = f'https://files.rcsb.org/download/{pdb_id.lower()}.{file_format}'
-response = requests.get(url)
+    Args:
+        pdb_id (str): The 4-character PDB ID of the structure to download.
+        output_dir (str): The directory where the downloaded file should be saved.
+        file_format (str): The format of the structure file ('pdb' or 'cif'). Default is 'pdb'.
 
-if response.status_code == 200:
-    with open(filename, 'w') as f:
-        f.write(response.text)
-    print(f'PDB structure saved as {filename}')
-else:
-    print(f"Failed to download structure for ID '{pdb_id.upper()}'. HTTP status code: {response.status_code}")
+    Returns:
+        None
+
+    Notes:
+        If the download is successful, the file is saved to the specified directory.
+        Otherwise, an error message is printed to the console.
+    """
+    url = f'https://files.rcsb.org/download/{pdb_id.lower()}.{file_format}'
+    response = requests.get(url)
+    filename = f'{pdb_id.upper()}.{file_format}'
+
+    if response.status_code == 200:
+        with open(f'{output_dir}/{filename}', 'w') as f:
+            f.write(response.text)
+        print(f'PDB structure saved as {filename}')
+    else:
+        print(
+            f"Failed to download structure for ID '{pdb_id.upper()}'. "
+            f'HTTP status code: {response.status_code}'
+        )
